983cb095933d69a47df9723b8a6ea585fd890318
kristoffer leal <89843271+DonLealGUA@users.noreply.github.com>
2024-10-27 17:05:43 +0100

    improvements

--- src/App.js
+++ src/App.js
@@ -5,65 +5,64 @@ import AddMovieForm from './components/AddMovieForm';
 import Movies from './components/Movies';
 import OrderByNameButton from './components/OrderByNameButton';
 import OrderByGradeButton from './components/OrderByGradeButton';
+import './App.css'; // Import custom CSS for styling
+
 function App() {
-    const [movies,setMovie] = useState([]); // State hook to manage movies array
+    const [movies, setMovie] = useState([]); // State hook to manage movies array
 
-// Adds a new movie to the list
-const addMovie = (newMovie) => {
-    setMovie([...movies,newMovie]);
-};
+    // Adds a new movie to the list
+    const addMovie = (newMovie) => {
+        setMovie([...movies, newMovie]);
+    };
 
-// Delete a movie from the list
-const deleteMovie = (movieToDelete) => {
-    setMovie(movies.filter(movie=> movie !== movieToDelete));
-};
+    // Delete a movie from the list
+    const deleteMovie = (movieToDelete) => {
+        setMovie(movies.filter((movie) => movie !== movieToDelete));
+    };
 
-// Orders movies by name 
-const orderByName = () => {
-    setMovie([...movies.sort((a,b) => a.title.localeCompare(b.title))]);
-};
+    // Orders movies by name
+    const orderByName = () => {
+        setMovie([...movies.sort((a, b) => a.title.localeCompare(b.title))]);
+    };
 
-// Orders movies by rating
-const orderByGrade = () => {
-    setMovie([...movies.sort((a,b) => b.rating - a.rating)]);
-};
+    // Orders movies by rating
+    const orderByGrade = () => {
+        setMovie([...movies.sort((a, b) => b.rating - a.rating)]);
+    };
 
-return(
-<Container>
-  <Row>
-    <Col>
-      <h1>ScreenScore</h1>
-    </Col>
-  </Row>
-  <Row>
-    <Col>
-      <AddMovieForm onAddMovie={addMovie} />
-    </Col>
-  </Row>
-  <Row>
-    <Col>
-      <h2 style={{ marginTop: '20px' }}>Inlagda Filmer</h2>
-    </Col>
-  </Row>
-  <Row>
-    <Col>
-      <Movies movies={movies} onDeleteMovie={deleteMovie} />
-    </Col>
-  </Row>
-  <Row>
-    <Col>
-      <div className="d-flex justify-content-between">
-        <OrderByNameButton onOrderByName={orderByName} />
-        <div style={{ width: '20px' }}></div> 
-        <OrderByGradeButton onOrderByGrade={orderByGrade} />
-      </div>
-    </Col>
-  </Row>
-</Container>
-
-);
-
-
+    return (
+        <Container className="app-container">
+            <Row className="text-center">
+                <Col>
+                    <h1 className="app-title">🎬 ScreenScore 🎬</h1>
+                </Col>
+            </Row>
+            <Row className="text-center">
+                <Col>
+                    <AddMovieForm onAddMovie={addMovie} />
+                </Col>
+            </Row>
+            <Row className="text-center">
+                <Col>
+                    <h2 className="app-subtitle">Inlagda Filmer</h2>
+                </Col>
+            </Row>
+            <Row className="text-center">
+                <Col>
+                    <Movies movies={movies} onDeleteMovie={deleteMovie} />
+                </Col>
+            </Row>
+            <Row className="text-center " style={{ marginTop: '30px' }}>
+                <Col>
+                  <div className="d-flex justify-content-between sort-button-container">
+                        <OrderByNameButton onOrderByName={orderByName} />
+                        <div style={{ width: '20px' }}></div> 
+                        <OrderByGradeButton onOrderByGrade={orderByGrade} />
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import { ListGroup } from 'react-bootstrap';
+import './Movie.css'; // Import the CSS file for styles
 
 const Movie = ({ movie, onDeleteMovie }) => {
     const { title, rating } = movie;
 
     return (
         <ListGroup.Item className="d-flex justify-content-between align-items-center">
-        <span>{title}</span>
-        <div>
-            {[...Array(parseInt(rating))].map((_, index) => ( //Array the size of the rating of the individual movie & also creates that amout of img objekts  
-                <img key={index} src="/media/star.png" alt="Star" className="star-icon" />
-            ))}
-            <img
-                src="/media/delete.png"
-                alt="Delete movie"
-                className="delete-movie-icon"
-                onClick={() => onDeleteMovie(movie)}
-            />
-        </div>
-    </ListGroup.Item>
+            <span>{title}</span>
+            <div>
+                {[...Array(parseInt(rating))].map((_, index) => ( // Create stars based on the rating
+                    <img 
+                        key={index} 
+                        src="/media/star.png" 
+                        alt="Star" 
+                        className="star-icon" 
+                    />
+                ))}
+                <img
+                    src="/media/delete.png"
+                    alt="Delete movie"
+                    className="delete-movie-icon"
+                    onClick={() => onDeleteMovie(movie)}
+                />
+            </div>
+        </ListGroup.Item>
     );
 };
 
